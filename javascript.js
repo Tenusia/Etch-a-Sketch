6c956fd7c0a5a724270b7108a6194e5e5f7ded95
Tenusia <110511640+Tenusia@users.noreply.github.com>
2023-08-10 14:24:15 +0200

    First commit
    -Set up grid generation from JS
    -Add CSS to change BG color on hover
    -Add JS eventListener to change BGC based on hover
    -Clear grid when creating a new grid
    -Clear BGC via button on main page

---
+++ javascript.js
@@ -0,0 +1,33 @@
+function generateGrid(nr){ 
+    //var body = document.body; // whatever you want to append the rows to: 
+    var body = document.querySelector('.grid')
+    if (body.hasChildNodes(row)) {body.replaceChildren()};
+
+    for(var i = 0; i < nr; i++){ 
+      var row = document.createElement("div"); 
+      row.className = "row"; 
+      for(var x = 1; x <= nr; x++){ 
+          var cell = document.createElement("div"); 
+          cell.className = "gridsquare"; 
+          cell.innerText = (i * nr) + x;
+          row.appendChild(cell); 
+      } 
+      body.appendChild(row); 
+    } 
+    //document.getElementById("code").innerText = body.innerHTML;
+  }
+
+function clearBGC(){
+    var cellNodeList = document.querySelectorAll('.gridsquare');
+    var i;
+    for (i = 0; i < cellNodeList.length; i++) {
+        cellNodeList[i].style.backgroundColor = "WHITE";
+    }
+}
+
+addEventListener("mouseover", function (e) {
+    console.log(e);
+    if(e.target.className == "gridsquare") {
+        e.target.style.backgroundColor = "RED";
+    }
+});
